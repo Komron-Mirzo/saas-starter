2710d95978fb2082fb2a1f973462389bffb9f968
Komron-Mirzo <74512232+Komron-Mirzo@users.noreply.github.com>
2026-09-09 23:34:20 +0500

    BookReaderSection updated

--- components/sections/home/BookReaderSection.tsx
+++ components/sections/home/BookReaderSection.tsx
@@ -12,7 +12,7 @@ export default function BookReaderSection() {
         <div className="max-w-[1560px] min-w-0 relative flex flex-col lg:flex-row items-end m-auto justify-between max-[1024px]:gap-[50px]">
           {/* Left Side: Exact Asset Dimensions Integration (Bubbles: 447x523, Worthy: 285x380) */}
           <div className="relative w-[42%] max-[1024px]:w-full max-[1024px]:max-w-[466px] m-auto flex flex-col items-center xl:items-start shrink-0">
-            <div className="relative w-full min-h-[749px] max-[1280px]:min-h-0 h-full max-w-full flex flex-col items-center justify-center">
+            <div className="relative w-full min-h-[749px] max-[1280px]:min-h-[600px] max-[1024px]:min-h-0 h-full max-w-full flex flex-col items-center justify-center">
 
               {/* SVG Bubbles containing the baked-in text */}
               <div className="absolute max-[1024px]:relative max-w-[447px] max-h-[523px] w-[23vw] h-[27vw] pointer-events-none right-0 top-0 max-[1024px]:w-[207px] max-[1024px]:h-[261px] max-[1024px]:ml-auto max-[1024px]:-mb-[84px]">
